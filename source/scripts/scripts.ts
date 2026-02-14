42bf4a77d851060487a742dc33bdce51200cc11e
Kira Ash Stephenson <SolaasAshenOnslaught@proton.me>
2026-02-13 22:59:35 -0800

    Redefined Terms, Created More CSS Constants, and Trimmed Some Elements

--- source/scripts/scripts.ts
+++ source/scripts/scripts.ts
@@ -7,18 +7,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Class Selectors for Block Types
     const blocks_1 = document.querySelectorAll<HTMLElement>(".block_1");
-    const blocks_2 = document.querySelectorAll<HTMLElement>(".block_2");
-    const blocks_3 = document.querySelectorAll<HTMLElement>(".block_3");
-    const blocks_4 = document.querySelectorAll<HTMLElement>(".block_4");
-    const blocks_5 = document.querySelectorAll<HTMLElement>(".block_5");
-    const blocks_6 = document.querySelectorAll<HTMLElement>(".block_6");
+    // const blocks_2 = document.querySelectorAll<HTMLElement>(".block_2");
+    // const blocks_3 = document.querySelectorAll<HTMLElement>(".block_3");
+    // const blocks_4 = document.querySelectorAll<HTMLElement>(".block_4");
+    // const blocks_5 = document.querySelectorAll<HTMLElement>(".block_5");
+    // const blocks_6 = document.querySelectorAll<HTMLElement>(".block_6");
 
     blocks_1.forEach(block => {
 
         block.addEventListener("click", () => {
 
-            if (block.style.backgroundColor != css_variables["color-1"]) {
-                block.style.backgroundColor = css_variables["color-1"];
+            if (block.style.backgroundColor != "white") {
+                block.style.backgroundColor = "white";
             }
             else {
                 block.style.backgroundColor = "green";
